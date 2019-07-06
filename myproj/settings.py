a702a8043cfac2f2b9c013ccccd3f0312d172688
myeongju <ko9608@naver.com>
2019-07-06 17:45:33 +0900

    Heroku 유치 준비

--- myproj/settings.py
+++ myproj/settings.py
@@ -120,3 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+#아래 코드는 heroku 상에서만 동작
+#몽키패칭
+import sys
+if 'win' not in sys.platform and 'darwin' != sys.platform:
+    import django_heroku
+    django_heroku.settings(locals())
